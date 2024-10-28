13695ddc42128aaff3439bdd20b499e45a3ef4df
Yanis Oulmane <152992744+YanOul1991@users.noreply.github.com>
2024-10-28 11:59:26 -0400

    Patch 0.0.2
    b

--- Assets/_Scripts/Paladin/CameraSuivit.cs
+++ Assets/_Scripts/Paladin/CameraSuivit.cs
@@ -2,13 +2,16 @@ using UnityEngine;
 
 public class CameraSuivit : MonoBehaviour
 {
-    public GameObject objCamera;
-    public Vector3 posCamera;
+    public GameObject objCamera; // Reference a l'objet camera
+    public Vector3 posCamera; // Distance de la camera par rapport au personnage
 
     // Update is called once per frame
     void Update()
     {
+        // Position la camera par rapport au personnage
         objCamera.transform.position = transform.position + posCamera;
+
+        // Oriente la camera pur qu'elle regarde toujours le personnage
         objCamera.transform.LookAt(transform.position);
     }
 }
--- Assets/_Scripts/Paladin/GestionPaladin.cs
+++ Assets/_Scripts/Paladin/GestionPaladin.cs
@@ -1,5 +1,4 @@
 using UnityEngine;
-
 /* 
 
 
@@ -10,32 +9,29 @@ public class GestionPaladin : MonoBehaviour
     /* =================================================================================== */
     /* ==================================== VARIABLES ==================================== */
     /* =================================================================================== */
-
     public float vDeplac; // Variable public float memorisant la vitesse de deplacement du personnage
-
-    public GameObject leRig; 
 
     // Start is called before the first frame update
     void Start()
     {
         // Verouille le curseur au centre de l'ecran
-        // Cursor.lockState = CursorLockMode.Locked;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {   
         // Applique les vitesses de deplacement du personnage
-        leRig.GetComponent<Rigidbody>().velocity = new Vector3(Input.GetAxisRaw("Horizontal"), leRig.GetComponent<Rigidbody>().velocity.y, Input.GetAxisRaw("Vertical")).normalized * vDeplac;
+        GetComponent<Rigidbody>().velocity = new Vector3(Input.GetAxisRaw("Horizontal"), GetComponent<Rigidbody>().velocity.y, Input.GetAxisRaw("Vertical")).normalized * vDeplac;
 
-        print(Input.GetAxisRaw("Horizontal"));
-        print(Input.GetAxisRaw("Horizontal"));
+        GetComponent<Animator>().SetBool("marche", GetComponent<Rigidbody>().velocity.magnitude > 5 ? true : false);
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        // Clique droit souris
+        if (Input.GetKeyDown(KeyCode.Mouse0))
         {
+            // Set le trigger de l'attaque
             GetComponent<Animator>().SetTrigger("attaque");
         }
 
-        gameObject.transform.position = leRig.transform.position;
     }
 }
