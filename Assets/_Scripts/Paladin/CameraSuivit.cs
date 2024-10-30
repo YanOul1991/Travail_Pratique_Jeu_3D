a5c56bd4d7a24a1203fd61890a3bcf544d4c2f90
Yanis Oulmane <152992744+YanOul1991@users.noreply.github.com>
2024-10-30 11:05:29 -0400

    0.0.4

--- Assets/_Scripts/Paladin/CameraSuivit.cs
+++ Assets/_Scripts/Paladin/CameraSuivit.cs
@@ -4,6 +4,7 @@ public class CameraSuivit : MonoBehaviour
 {
     public GameObject objCamera; // Reference a l'objet camera
     public Vector3 posCamera; // Distance de la camera par rapport au personnage
+    public float VitesseTourne;
 
     void Start()
     {
@@ -13,8 +14,11 @@ public class CameraSuivit : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        objCamera.transform.RotateAround(transform.position,  Vector3.up, Input.GetAxis("Mouse X") * VitesseTourne);
+
         // Position la camera par rapport au personnage
-        objCamera.transform.position = transform.position + posCamera;
+        // objCamera.transform.position = transform.TransformPoint(posCamera);
 
         // Oriente la camera pur qu'elle regarde toujours le personnage
         objCamera.transform.LookAt(transform.position);
--- Assets/_Scripts/Paladin/DeplacementPaladin.cs
+++ Assets/_Scripts/Paladin/DeplacementPaladin.cs
@@ -2,33 +2,103 @@ using UnityEngine;
 
 public class DeplacementPaladin : MonoBehaviour
 {
-    public float VitesseDeplacement;
-    public float VitesseTourne;
+    /* ================================================================================================= */
+    /* ================================== VARIABLES POUR DEPLACEMENTS ================================== */
+    /* ================================================================================================= */
+    public float vitesseMarche; // Vitesse de deplacement;
+    public float vitesseSprint; // Vitesse du sprint du personnage;
+    float vitesseReel; // Vitesse qui sera applique au personnage
+    public float forceSaut; // Force du saut
+    public float forceGravite; // Force de la gravite
+    float velociteY; // Velocite en Y 
+    bool saut; // Bool memorisant si le personnage a saute
+    bool auSol; // Bool memorisant si le personnage est au sol
 
+    /* =================================================================================================== */
     /* ================================= VARIABLES REFERENCES COMPONENTS ================================= */
+    /* =================================================================================================== */
     private CharacterController controleur; // Variable private CharcterController pour faire plus rapidement reference au character controller du personnage
 
     // Start is called before the first frame update
     void Start()
     {
-        // Assignation des Components
+        Cursor.lockState = CursorLockMode.Locked;
+
+        // Assignations des references aux components du personnage
         controleur = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Cherche les valeurs de l'axe vertical et horizontal
-        Vector3 valeursInput = Vector3.zero;
-        valeursInput.x = Input.GetAxis("Horizontal");
-        valeursInput.z = Input.GetAxis("Vertical");
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            saut = true;
+        }
+
+        GetComponent<Animator>().SetBool("saut", !auSol);
         
-        // Applique les forces de deplacement aux axes du monde
-        Vector3 deplacement = transform.TransformDirection(valeursInput * VitesseDeplacement);
+        // La vitesse reel qui sera applique au personnage dependra de selon si la touche shift est maintenu
+        // Si oui la vitesse sera celle du sprint, sinon sera celle de la marche;
+        vitesseReel = Input.GetKey(KeyCode.LeftShift) ? vitesseSprint : vitesseMarche;
+    }
 
-        controleur.SimpleMove(deplacement);
+    void FixedUpdate()
+    {
+        Vector3 deplacement = transform.TransformDirection(new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical")) * vitesseReel);
 
-        // Faire tourner le personnage en fonction du deplacement horizontal de la souris
-        transform.Rotate(0f, Input.GetAxis("Mouse X") * VitesseTourne, 0f);
+        // Verifie si le personnage est au sol avec un SphereCast
+        RaycastHit infoCollision;
+        auSol = Physics.SphereCast(transform.position + new Vector3(0, 0.5f, 0), 0.5f, Vector3.down, out infoCollision, 0.5f);
+
+
+        // Si le personnage touche au sol et que la velocite est plus petit que 0
+        if (auSol && velociteY < 0)
+        {
+            // Remet la velocite en Y a 0
+            velociteY = 0f;
+        }
+
+        // Si active le saut et que le personnage touche au saul
+        if (saut && auSol)
+        {
+            // Applique comme velocite en y la force de saut
+            velociteY = forceSaut;
+            saut = false;
+        }
+
+        // Applique la force de gravite a la velociteY du personnage a chaque seconde pour lui permetttre de retomber
+        velociteY += forceGravite * Time.deltaTime;
+
+        // Applique la velocite en Y au deplacement du personnage
+        deplacement.y = velociteY;
+
+        // Applique lews deplacements du personnage multiplie par Time.deltaTime pour un variation constante
+        controleur.Move(deplacement * Time.deltaTime);
+
+        GestionAnimations(deplacement);
+    }
+
+    void GestionAnimations(Vector3 valeurDeplacement)
+    {
+        // Applique un valeur de zero a la valeur du deplacement en y
+        // pour seulement verifier les mouvement sur les axes X et Y
+        valeurDeplacement.y = 0;
+
+        if (vitesseReel == vitesseMarche)
+        {
+            GetComponent<Animator>().SetBool("sprint", false);
+
+            // Set le bool soit a true ou false selon si la valeur de deplacement sur les axes x et z est superieur a 1f
+            GetComponent<Animator>().SetBool("marche", valeurDeplacement.magnitude > 1f ? true : false);
+        }
+
+        if (vitesseReel == vitesseSprint)
+        {
+            GetComponent<Animator>().SetBool("marche", false);
+
+            // Set le bool soit a true ou false selon si la valeur de deplacement sur les axes x et z est superieur a 1f
+            GetComponent<Animator>().SetBool("sprint", valeurDeplacement.magnitude > 1f ? true : false);
+        }
     }
 }
